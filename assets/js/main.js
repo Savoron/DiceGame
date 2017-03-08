fc6d24e4f06b625969303f65fc2a1a4ca713a7e7
Travis <t.j.palmer@comcast.net>
2017-03-08 09:03:47 -0600

    Added orbit camera. Idk if I did it correctly though.. whoops..

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,25 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+		camera.position.z = 5;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+var renderer = new THREE.WebGLRenderer( { antialias: true } );
+		renderer.setPixelRatio( window.devicePixelRatio );
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		renderer.setClearColor( 0x000000, 1 );
+		document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var orbit = new THREE.OrbitControls( camera, renderer.domElement );
 
-camera.position.z = 5;
+var geometry = new THREE.ConeBufferGeometry( 2, 3, 3 );
+var material = new THREE.MeshBasicMaterial( {color: 0x00aaff} );
+var cone = new THREE.Mesh( geometry, material );
+scene.add( cone );
+
 
 var render = function () {
 	requestAnimationFrame( render );
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
+	cone.rotation.y += 0.01;
 
 	renderer.render(scene, camera);
 };
